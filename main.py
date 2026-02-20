dc13be3cf35eb69f78e9f6b60abe9f3083bc0f55
AlexPN48 <apn48@alu.ua.es>
2026-02-20 10:27:35 +0100

    mainprueba

---
+++ main.py
@@ -0,0 +1,16 @@
+class Pista:
+
+    def __init__(self, km):
+        self.km = km
+
+class Ciclista:
+
+    def __init__(self, nombre, apellidos, edad):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.edad = edad
+
+
+
+if __name__ == '__main__':
+    print()
